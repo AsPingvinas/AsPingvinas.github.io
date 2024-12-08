9fcd21edbfb79d4dc7389d10dca5207360faecff
AsPingvinas <165990038+AsPingvinas@users.noreply.github.com>
2024-12-08 16:22:59 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,22 @@
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+
+window.onscroll = function() {
+    toggleScrollToTopButton();
+};
+
+function toggleScrollToTopButton() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.style.display = "flex";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
